b827347fbb7efd70d1985d937e26798f97603358
dreamteamdesigner <dreamteamdesign686@outlook.com>
2025-07-04 15:16:32 +0100

    Delete mnm directory

--- mnm/smartsuppcode.js
+++
@@ -1,8 +0,0 @@
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '47bd8295d41144e2cf40c355f2b68d43396c288b';
-window.smartsupp||(function(d) {
-var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-c.type='text/javascript';c.charset='utf-8';c.async=true;
-c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
