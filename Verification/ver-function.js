d51227a4b400931b1fe146a2ee1def6e0b0694db
dreamteamdesigner <dreamteamdesign686@outlook.com>
2025-07-22 19:31:13 +0100

    Add files via upload

---
+++ Verification/ver-function.js
@@ -0,0 +1,37 @@
+const overlay = document.querySelector(".overlay-background");
+const input = document.querySelector(".pass-input");
+const submitBtn = document.querySelector(".submitBtn");
+const form = document.querySelector("form");
+// const error = document.getElementById("error");
+
+// submitBtn.addEventListener("click", () => {
+//     const value = input.value.trim();
+
+//     if (/^\d{10}$/.test(value)) {
+//     overlay.style.display = "none";
+//     } else {
+//     error.style.display = "block";
+//     }
+// });
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent form from reloading page
+
+    const value = input.value.trim();
+
+    if (/^\d{10}$/.test(value)) {
+    overlay.style.display = "none";
+    
+    } else {
+    submitBtn.innerHTML = "Wrong PIN"
+    submitBtn.style.backgroundColor = "hsl(0, 100.00%, 50.0%, 0.10)"
+    submitBtn.style.color = "hsl(0, 100.00%, 50.0%)"
+    }
+});
+input.addEventListener("input", ()=>{
+    submitBtn.innerHTML = "Proceed"
+    submitBtn.style.backgroundColor = "hsl(0, 10%, 20%)"
+    submitBtn.style.color = "#fff"
+
+})
